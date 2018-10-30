216c44bbbea5a710c263b08655a5b68342be6a84
Jan Ritter <jan-ritter@outlook.com>
2018-10-30 21:47:33 +0100

    refactor: moved tower configuration from dynamodb into lambda env vars

--- config/config.go
+++ config/config.go
@@ -1,15 +1,10 @@
 package config
 
 import (
-	"log"
 	"os"
 	"strconv"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/lambda"
 	lightning "github.com/janritter/go-lightning-log"
-	"gitlab.com/janritter/auto-staging-tower/types"
 )
 
 var Logger *lightning.Lightning
@@ -17,27 +12,4 @@ var Logger *lightning.Lightning
 func Init() {
 	logLevel, _ := strconv.Atoi(os.Getenv("CONFIGURATION_LOG_LEVEL"))
 	Logger, _ = lightning.Init(logLevel)
-}
-
-func UpdateLambdaConfiguration(configuration types.TowerConfiguration) {
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("eu-central-1")},
-	)
-
-	if err != nil {
-		log.Println(err)
-	}
-
-	svc := lambda.New(sess)
-
-	_, err = svc.UpdateFunctionConfiguration(&lambda.UpdateFunctionConfigurationInput{
-		FunctionName: aws.String("auto-staging-tower"),
-		Environment: &lambda.Environment{
-			Variables: map[string]*string{"CONFIGURATION_LOG_LEVEL": aws.String(strconv.Itoa(configuration.LogLevel))},
-		},
-	})
-
-	if err != nil {
-		log.Println(err)
-	}
 }
--- model/configuration.go
+++ model/configuration.go
@@ -1,73 +1,59 @@
 package model
 
 import (
+	"os"
 	"strconv"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/lambda"
 	"gitlab.com/janritter/auto-staging-tower/config"
 	"gitlab.com/janritter/auto-staging-tower/types"
 )
 
-func GetConfiguration(configuration *types.TowerConfiguration, stage string) error {
-	svc := getDynamoDbClient()
+func GetConfiguration(configuration *types.TowerConfiguration) error {
 
-	result, err := svc.GetItem(&dynamodb.GetItemInput{
-		TableName: aws.String("auto-staging-tower-conf"),
-		Key: map[string]*dynamodb.AttributeValue{
-			"towerStage": {
-				S: aws.String(stage),
-			},
-		},
-	})
-
+	logLevel, err := strconv.Atoi(os.Getenv("CONFIGURATION_LOG_LEVEL"))
 	if err != nil {
-		config.Logger.Log(err, map[string]string{"module": "model/GetConfiguration", "operation": "db/execution"}, 0)
+		config.Logger.Log(err, map[string]string{"module": "model/GetConfiguration"}, 1)
 		return err
 	}
 
-	err = dynamodbattribute.UnmarshalMap(result.Item, configuration)
-	if err != nil {
-		config.Logger.Log(err, map[string]string{"module": "model/GetConfiguration", "operation": "db/unmarshal"}, 1)
-		return err
-	}
+	configuration.LogLevel = logLevel
 
 	return nil
 }
 
-func UpdateConfiguration(configuration *types.TowerConfiguration, stage string) error {
-	svc := getDynamoDbClient()
+func UpdateConfiguration(configuration *types.TowerConfiguration) error {
+	sess, errSession := session.NewSession(&aws.Config{
+		Region: aws.String("eu-central-1")},
+	)
 
-	input := &dynamodb.UpdateItemInput{
-		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
-			":logLevel": {
-				N: aws.String(strconv.Itoa(configuration.LogLevel)),
-			},
-		},
-		TableName: aws.String("auto-staging-tower-conf"),
-		Key: map[string]*dynamodb.AttributeValue{
-			"towerStage": {
-				S: aws.String(stage),
-			},
-		},
-		ReturnValues:     aws.String("UPDATED_NEW"),
-		UpdateExpression: aws.String("SET logLevel = :logLevel"),
+	if errSession != nil {
+		config.Logger.Log(errSession, map[string]string{"module": "model/UpdateConfiguration", "operation": "aws/session"}, 0)
+		return errSession
 	}
 
-	config.UpdateLambdaConfiguration(*configuration)
+	svc := lambda.New(sess)
 
-	result, err := svc.UpdateItem(input)
+	result, err := svc.UpdateFunctionConfiguration(&lambda.UpdateFunctionConfigurationInput{
+		FunctionName: aws.String("auto-staging-tower"),
+		Environment: &lambda.Environment{
+			Variables: map[string]*string{"CONFIGURATION_LOG_LEVEL": aws.String(strconv.Itoa(configuration.LogLevel))},
+		},
+	})
+
 	if err != nil {
-		config.Logger.Log(err, map[string]string{"module": "model/UpdateConfiguration", "operation": "db/execution"}, 0)
+		config.Logger.Log(err, map[string]string{"module": "model/UpdateConfiguration", "operation": "lambda/update_config"}, 0)
 		return err
 	}
 
-	err = dynamodbattribute.UnmarshalMap(result.Attributes, configuration)
+	logLevel, err := strconv.Atoi(*result.Environment.Variables["CONFIGURATION_LOG_LEVEL"])
 	if err != nil {
-		config.Logger.Log(err, map[string]string{"module": "model/UpdateConfiguration", "operation": "db/unmarshal"}, 1)
+		config.Logger.Log(err, map[string]string{"module": "model/GetConfiguration", "operation": "lambda/update_config_result"}, 1)
 		return err
 	}
+	configuration.LogLevel = logLevel
 
-	return err
+	return nil
 }
