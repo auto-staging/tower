7f52957665efbf4f393f4b1dbfd8c48ab253a522
Jan Ritter <jan-ritter@outlook.com>
2018-12-25 06:28:19 +0100

    docs: added go doc for logger config

--- config/config.go
+++ config/config.go
@@ -7,8 +7,10 @@ import (
 	lightning "github.com/janritter/go-lightning-log"
 )
 
+// Logger contains the Lightning Logger instance configured by the Init function, it's used for logging by calling the Log function on it.
 var Logger *lightning.Lightning
 
+// Init is used to initalize Lightning Logger with the configured LogLevel.
 func Init() {
 	logLevel, _ := strconv.Atoi(os.Getenv("CONFIGURATION_LOG_LEVEL"))
 	Logger, _ = lightning.Init(logLevel)
