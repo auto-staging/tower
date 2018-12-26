1e4be04000c5a8a1af6be2630da8e74b51b073c3
Jan Ritter <jan-ritter@outlook.com>
2018-12-26 03:44:58 +0100

    refactor: removed unused alias for import

--- config/config.go
+++ config/config.go
@@ -4,7 +4,7 @@ import (
 	"os"
 	"strconv"
 
-	lightning "github.com/janritter/go-lightning-log"
+	"github.com/janritter/go-lightning-log"
 )
 
 // Logger contains the Lightning Logger instance configured by the Init function, it's used for logging by calling the Log function on it.
